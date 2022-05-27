f5ca1dd3510d21d453911f27ffeab6a7dd64854c
Deepak Mahakale <deepakmahakale@gmail.com>
2022-05-27 18:00:48 +0530

    Add error controller

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,5 @@
+class ErrorsController < ApplicationController
+  def index
+    10 / 0
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :errors, only: :index
 end
